623672ed99ce4b06abb9c709d423d7892e8660f3
mzhu1 <mzhu1@illumina.com>
2019-02-01 16:04:45 +0000

    finish iam delete_user.py

--- functions/iam_delete_user.md
+++ functions/iam_delete_user.md
@@ -1,1 +1,20 @@
-In order to have a clean delete it is necessary the following iam requirements:<br>
+This function delete iam user.
+
+Function that we can use to delete iam user
+* delete_user_with_profile(profile, region, user)
+* delete_user_with_instance_role(user)
+* delete_user_with_new_role(user, new_role_arn, region)
+
+Requirements to delete the user:
+* iam:DeactivateMFADevice<br>
+* iam:DeleteAccessKey<br>
+* iam:RemoveUserFromGroup<br>
+* iam:DeleteVirtualMFADevice<br>
+* iam:ListAttachedUserPolicies<br>
+* iam:DeleteUser<br>
+* iam:ListMFADevices<br>
+* iam:DetachUserPolicy<br>
+* iam:TagUser<br>
+* iam:DeleteLoginProfile<br>
+* iam:ListUserTags<br>
+* iam:ListAccessKeys<br>
--- functions/iam_delete_user.py
+++ functions/iam_delete_user.py
@@ -2,33 +2,63 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-def delete_user_from_group(iam_session, user):
+def delete_user(iam_session, user):
+    try:
+        iam_session.delete_user(UserName=user)
+    except ClientError as ex:
+        print(ex)
+
+
+def delete_mfa(iam_session, user):
+    try:
+        for mfa in iam_session.list_mfa_devices(UserName='user_delete')['MFADevices']:
+            iam_session.deactivate_mfa_device(UserName=user, SerialNumber=mfa['SerialNumber'])
+            iam_session.delete_virtual_mfa_device(SerialNumber=mfa['SerialNumber'])
+    except ClientError as ex:
+        print(ex)
+
+
+def delete_access_keys(iam_session, user):
+    try:
+        for access_key in iam_session.list_access_keys(UserName=user)['AccessKeyMetadata']:
+            iam_session.delete_access_key(UserName=user, AccessKeyId=access_key['AccessKeyId'])
+    except ClientError as ex:
+        print(ex)
+
+
+def detach_user_from_policies(iam_session, user):
+    try:
+        for policy in iam_session.list_attached_user_policies(UserName=user)['AttachedPolicies']:
+            boto3.client('iam').detach_user_policy(UserName=user, PolicyArn=policy['PolicyArn'])
+    except ClientError as ex:
+        print(ex)
+
+
+def detach_user_from_groups(iam_session, user):
     try:
         for group in iam_session.list_groups_for_user(UserName=user)['Groups']:
-            iam_session.remove_user_from_group(GroupName=group['GroupName'], UserName=user)
-    except ClientError:
-        print('No User From Group')
+            iam_session.remove_user_from_group(UserName=user, GroupName=group['GroupName'])
+    except ClientError as err:
+        print(err)
 
 
 def delete_login_profile(iam_session, user):
     try:
         iam_session.delete_login_profile(UserName=user)
-    except ClientError:
-        print('No Login Profile')
+    except ClientError as err:
+        print(err)
 
 
 def function_delete_user(iam_session, user):
     delete_login_profile(iam_session, user)
-    delete_user_from_group(iam_session, user)
-    # # To be Fixed
-    # iam_session.detach_user_policy(UserName=user, PolicyArn='')
-    # iam_session.delete_access_key(UserName=user, AccessKeyId='')
-    # iam_session.deactivate_mfa_device(UserName=user, SerialNumber='')
-    # iam_session.delete_virtual_mfa_device(SerialNumber='')
-    # iam_session.delete_user(UserName=user)
+    detach_user_from_groups(iam_session, user)
+    detach_user_from_policies(iam_session, user)
+    delete_access_keys(iam_session, user)
+    delete_mfa(iam_session, user)
+    delete_user(iam_session, user)
 
 
-def delete_user_with_profile_iam(profile, region, user):
+def delete_user_with_profile(profile, region, user):
     session = boto3.Session(profile_name=profile, region_name=region)
     iam_session = session.client('iam')
     function_delete_user(iam_session, user)
